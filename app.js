5c389acfa00e0e94ecfeea3470174fd94c960587
Sourabhtripathi <sourabh.tripathi48@gmail.com>
2019-01-12 23:11:24 +0530

    set env variables

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get("/", (req, res)=>{
 	res.render("index");
 });
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, process.env.IP, ()=>{
 	console.log("App is now listening on port 3000");
 });
